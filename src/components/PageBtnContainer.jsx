c916a501c23c6db5e112c5a0d093ecf9c8539cf2
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-06-17 07:22:39 +0100

    pageBtnContainer structure

--- src/components/PageBtnContainer.jsx
+++ src/components/PageBtnContainer.jsx
@@ -1,8 +1,46 @@
-
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
+import Wrapper from '../assets/wrappers/PageBtnContainer'
+import { useSelector, useDispatch } from 'react-redux'
 
 const PageBtnContainer = () => {
+  const { numOfPages, page } = useSelector((store) => store.allJobs)
+  const dispatch = useDispatch()
+
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1
+  })
+  const nextPage = () => {
+    console.log('Next Page')
+  }
+  const prevPage = () => {
+     console.log('Prev Page')
+  }
+
   return (
-    <h2>PageBtnContainer</h2>
+    <Wrapper>
+      <button type='button' className='prev-btn' onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        Prev
+      </button>
+      <div className='btn-container'>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              type='button'
+              className={pageNumber === page ? 'pageBtn active' : 'pageBtn'}
+              onClick={()=>console.log('change page')}
+            >
+              {pageNumber}
+            </button>
+          )
+        })}
+      </div>
+      <button className='next-btn' onClick={nextPage}>
+        Next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
   )
 }
 
